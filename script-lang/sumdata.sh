eec62b488f75abef6b34a955ffdaa80a5f8d4396
Markus Bergholz <markuman@gmail.com>
2014-06-07 20:10:30 +0000

    add comment to bash script + add rule in readme

--- script-lang/sumdata.sh
+++ script-lang/sumdata.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # time ./script-lang/sumdata.sh data.log
+# yes, that's all binary "bash" functiones ;)
 
 original=$(cat "$1" | awk '{print$5}'|paste -sd+ | bc)
 printf 'Original size %s kb\n' "$(($original/1000))"
