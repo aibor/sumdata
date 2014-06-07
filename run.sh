d0f0df83fc024d16e43bb516c2b7b605b1b29c6b
Markus Bergholz <markuman@gmail.com>
2014-06-07 22:00:02 +0200

    add author

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # bash
-printf '\n==== BASH ====\n'
+printf '\n==== BASH ====\nAuthor: github.com/markuman\n'
 time ./script-lang/sumdata.sh data.log
 
 # lua
-printf '\n==== LUA ====\n'
+printf '\n==== LUA ====\nAuthor: github.com/markuman\n'
 time ./script-lang/sumdata.lua data.log
 
 # luajit
-printf '\n==== LUAJIT ====\n'
+printf '\n==== LUAJIT ====\nAuthor: github.com/markuman\n'
 time luajit ./script-lang/sumdata.lua data.log
 
